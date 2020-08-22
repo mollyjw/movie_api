fee632ef8df1de6a6098f60f706c5df198bef6f4
MollyJW <mollyjwhitener@gmail.com>
2020-08-22 13:43:34 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,31 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :movies
-  resources :titles
-  resources :users
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      namespace :users do
+        post :login
+        post :create
+        get :me
+        delete :logout
+      end
+
+      namespace :movies do
+        get :index
+        get :show
+        post :create
+        patch :update
+        delete :destroy
+        get :get_upload_credentials
+      end
+
+      namespace :reviews do
+        get :index
+        get :show
+        post :create
+        patch :update
+        delete :destroy
+      end
+    end
+  end
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
